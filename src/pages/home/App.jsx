106badf733fbeff3ecfef5d59531fd5e84f2d6e0
nandopuntodewo <deanandropuntodewo@students.unnes.ac.id>
2025-07-25 22:30:29 +0700

    update navbar

--- src/pages/home/App.jsx
+++ src/pages/home/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import Navbar from '../../components/global/Nav';
 
 function App() {
 	return (
     <>
+     <div>
+      <Navbar />
       <p>Hello Pak Ndut anjay</p>
+      </div>
     </>
   );
 }
