660be152d82babcf367ef3501fa10afd52e1c853
nandopuntodewo <deanandropuntodewo@students.unnes.ac.id>
2025-07-26 01:05:04 +0700

    auah

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,9 @@ import App from './App.jsx'
 import './index.css'
 import "@fontsource/montserrat";
 import Footer from "./components/global/Foot";
-// import Navbar from "./components/global/Nav";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		{/* <Navbar /> */}
 		<App />
 		<Footer />
 	</React.StrictMode>
