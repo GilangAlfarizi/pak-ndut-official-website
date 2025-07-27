40dc2e32ba6ce51f7f6eaed545d49e563600b15a
dylansanjaya <dylanhikmasanjaya@gmail.com>
2025-07-27 20:34:53 +0700

    Add Home Component Folder

---
+++ src/components/home/AboutUs.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+  return(
+    <div className="bg-[#BA0202]">
+      <div className="grid grid-cols-2 grid-rows-3">
+        tes
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../../components/global/Nav";
+import About from "../../components/home/AboutUs";
 
 const LandingPage = () => {
-    return (
+  return (
     <>
-     <div>
+      <div>
         <Navbar />
-      <p>Hello Pak Ndut anjay</p>
+        <p>Hello Pak Ndut anjay</p>
+        <About />
       </div>
     </>
   );
