1b728098d413c576ba3be40135ba4eedef833316
nandopuntodewo <deanandropuntodewo@students.unnes.ac.id>
2025-07-27 22:15:32 +0700

    hero jsx

---
+++ src/components/home/hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import hero from "../../../public/images/hero.svg";
+
+const Hero = () => {
+  return (
+    <div className="relative w-full h-screen overflow-hidden">
+      <img
+        className="absolute inset-0 object-cover w-full h-full brightness-40"
+        src={hero}
+        alt="Hero Image"
+      />
+      <div className="absolute inset-0 flex flex-col items-center justify-center space-y-4 p-2">
+        <h1 className="text-6xl text-white font-black text-center">
+          Classic Taste, Modern Twist.
+        </h1>
+        <h1 className="text-6xl text-white font-black text-center">
+          Fried duck the Indonesian way.
+        </h1>
+        <button className="mt-4 px-10 py-4 bg-[#BA0202] text-white text-2xl font-bold rounded-[40px] hover:bg-red-600 transition duration-200">
+          Whatsapp Franchise
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "../../components/global/Nav";
 import About from "../../components/home/AboutUs";
-
+import Hero from "../../components/home/hero";
 const LandingPage = () => {
   return (
     <>
       <div>
         <Navbar />
+        <Hero />
         <p>Hello Pak Ndut anjay</p>
         <About />
       </div>
